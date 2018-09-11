0ed8a23db988bfda3eb1557a1e4627a6bb1fe9a2
= <christiano.piccinin@gmail.com>
2018-09-11 16:17:57 -0300

    commit alteracoes

--- front-end/controller/relatorio.js
+++
@@ -1,9 +0,0 @@
-var relatorioModulo =  angular.module('relatorioModulo',[]);
-
-
-relatorioModulo.controller("relatorioController",function ($scope, $http) {
-    $http.get('controller/relatoriojson.json').then(function (response) {
-        $scope.registros = response.data.relatorio;
-    })
-
-});
